e7e1b6bfb4396709ab2e671f10845ec5992d4a0a
Maciej Ostaszewski <maco@netcompany.com>
2021-03-23 20:07:55 +0100

    add merged data

--- data_covid.R
+++ data_covid.R
@@ -1,18 +1,35 @@
+rm(list = ls())
 library(data.table) 
 library(dplyr)
 library(stringi)
-setwd("/home/kisiel/Covid-and-air-pollution/") 
 
-dir_path <- "dane/danehistorycznepowiaty/"
+
+dir_path <- "path"
 files <- list.files(path=dir_path, pattern = "pow")
 
-dane <- rbindlist(lapply(files, function(file){
-  dt <- read.csv2(paste0(dir_path,"/",file), colClasses = "character", encoding = "latin1") #[,c(1:16)]
+dane1 <- rbindlist(lapply(files, function(file){
+  dt <- read.csv2(paste0(dir_path,"/",file), colClasses = "character", encoding = "UTF-8")  #[,c(1:16)]
+  date <- substr(file,1,8)
+  y <- substr(date,1,4)
+  m <- substr(date,5,6)
+  d <- substr(date,7,8)
+  
+  dt <- dt %>%
+    mutate(stan_rekordu_na=paste0(y,"-",m,"-",d))
   
   }),use.names = TRUE, fill = TRUE)
+#(dane, "dane1.csv")
+
+
+dane <- read.csv2("dane/merge_data.csv", sep = ",", encoding = "UTF-8")
+
+dict <- read.csv2("dane/slownik_powiaty.csv", sep = ",", encoding = "UTF-8")
 
 dane <- dane %>% 
   filter(!(is.na(stan_rekordu_na))) %>%
   mutate(ow = substr(teryt,2,3))
 
-dict <- read.csv2("dane/slownik_powiaty.csv", sep = ",", encoding = "windows-1250")
+dane <- dane %>% 
+  mutate(teryt_num = substr(teryt,2,3))
+
+
