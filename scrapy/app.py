83104956dca173822af25cf2ef353015d115904e
cnuebred <jonatann01ew@gmail.com>
2021-03-24 21:17:54 +0100

    scrapy app v2

--- scrapy/app.py
+++ scrapy/app.py
@@ -1,32 +1,47 @@
 import requests
 import json
+import multitasking
 from statistics import mean
 import pandas as pd
 
 requests_ = requests.Session()
 
 stations = requests_.get("http://api.gios.gov.pl/pjp-api/rest/station/findAll")
-
 stations = json.loads(stations.content)
 
+is_first = True
 
-def get_sensors(sensors):
+
+@multitasking.task
+def save(station, sensor, values):
+    global is_first
+    book = {}
+    book["geg_lat"] = station.get("gegrLat")
+    book["geg_lon"] = station.get("gegrLon")
+    book["district_name"] = station.get("city").get("commune").get("districtName")
+    book["province_name"] = station.get("city").get("commune").get("provinceName")
+    book["param_name"] = sensor.get("param").get("paramName")
+    book["param_formula"] = sensor.get("param").get("paramFormula")
+    book["values"] = values.get("values")
+
+    columns = None
+    if is_first:
+        columns = list(book.keys())
+        is_first = False
+
+    df = pd.DataFrame([list(book.values())])
+
+    df.to_csv("file.csv", mode="a", header=columns, encoding="utf-8")
+
+
+def get_sensors(station, sensors):
     for sensor in sensors:
         sensorid = sensor.get("id")
         values = requests_.get(
             f"http://api.gios.gov.pl/pjp-api/rest/data/getData/{sensorid}"
         )
         values = json.loads(values.content)
-        # print(
-        #     values.get("key"),
-        #     mean(
-        #         [
-        #             data.get("value")
-        #             for data in values.get("values")[:24]
-        #             if data.get("value")
-        #         ]
-        #     ),
-        # )
+        save(station, sensor, values)
 
 
 def main():
@@ -36,7 +51,8 @@ def main():
             f"http://api.gios.gov.pl/pjp-api/rest/station/sensors/{stationid}"
         )
         sensors = json.loads(sensors.content)
-        get_sensors(sensors)
+        get_sensors(station, sensors)
 
 
 main()
+
